ed26e9ccf7c7ff2b6522cc55006288e2b8779ec9
gheor <gheor@DESKTOP-0TQLUER>
2021-02-26 17:50:28 +0200

    adaugare clase din diagrama

--- Seminar1/src/pachet1/Program.java
+++ Seminar1/src/pachet1/Program.java
@@ -1,8 +1,19 @@
 package pachet1;
 
+import clase.Girafa;
+import clase.Zoo;
+
 public class Program {
 
-	public static void main(String [] args) {
-
+	public static void main(String[] args) {
+		Zoo zoo = new Zoo();
+	Girafa g1 = new Girafa("g1");
+	Girafa g2 = new Girafa("g2");
+	
+	zoo.AdaugaAnimal(g1);
+	zoo.AdaugaAnimal(g2);
+	
+	zoo.FeedAnimals();
+	
 	}
 }
